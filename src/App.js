a345ff7ba9c2defc38501ffa72e197b21b75cde0
Joseph-K-B <303josephb@gmail.com>
2021-08-02 16:28:41 -0700

    basic file structure

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src="./assets/fb-icon.png" alt="facebook" />
+        <img src="./assets/twit-icon.png" alt="twitter" />
+        <img src="./assets/gp-icon.png" alt="google+" />
+        <img src="./assets/insta-icon.png" alt="insta" />
+        <img src="./assets/flic-icon.png" alt="flickr" />
+        <img src="./assets/pint-icon.png" alt="pinterest" />
+        <img src="./assets/rss-icon.png" alt="rss" />
+        <img src="./assets/mail-icon.png" alt="mail" />
       </header>
+      <main>
+        <p>
+        <img src="./assets/choco-pizza.png" alt="chocolate-pizza" id="choco-pizza" />
+          <descriptParagraph par1='p1' body='Choco'/>
+        </p>
+        <p>
+          <recipeParagraph par2='p2' body='Pizza'/>
+        </p>
+        </main>
+        <footer>
+        <img src="./assets/small-logo.png" alt="small knife/fork" id="footer-pic" />
+        </footer>
     </div>
   );
 }
